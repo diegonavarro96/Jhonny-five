4b0d5606ea42f67e1e519da6befed66ee5c7f256
diegonavarro96 <diegonavarroarizpe@hotmail.com>
2020-02-15 23:17:59 -0600

    First chart attempt

--- LED/public/LEDHandle.js
+++ LED/public/LEDHandle.js
@@ -2,11 +2,13 @@
 
 const socket = io.connect('http://localhost:3000');
 
+
 //Query DOM
 const blink = document.getElementById('blink');
 const on = document.getElementById('on');
 const off = document.getElementById('off');
 const pwm = document.getElementById('pwm');
+const start = document.getElementById('start');
 
 
 //Emit events
@@ -25,7 +27,38 @@ on.addEventListener('click',function(){
 off.addEventListener('click',function(){
     socket.emit('off','off')
 });
-var x =0;
-var cnt =0;
-
+let x = 0;
 //listen for events
+start.addEventListener('click',function(){
+    if(x === 2){
+        x = 0;
+    }
+
+    const myChart = document.getElementById('lineChart');
+let xAxis = [2,3,4,5,6,7,8,9,10]
+let yAxis =[[0,1,2,3,4,5,6,7,8,9,10,11,12],[11,12,13,14,15]]
+let chartData = {
+    labels: xAxis,//X axis
+    datasets: [{ 
+        data: yAxis[x],//Y axis
+        label: "Motor",
+        borderColor: "#3e95cd",
+        fill: false
+      }
+    ]
+  }
+
+let massPopChart = new Chart(myChart,{
+    type:'line',
+    data: chartData,
+    options: {
+        title: {
+          display: true,
+          text: 'World population per region (in millions)'
+        }
+      }
+
+})
+x++;
+
+})
